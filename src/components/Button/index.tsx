c362b1370b582439024aa4b722741cc2c5418156
Arunkumar Selvaraj <arun@ioak.io>
2023-01-13 12:05:36 +0530

    support callbacks on button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,6 +5,9 @@ import "./style.css";
 export interface ButtonProps {
     theme?: ThemeType;
     onClick?: any;
+    onSubmit?: any;
+    onReset?: any;
+    type?: 'button' | 'submit' | 'reset';
     children: any;
 };
 
@@ -12,13 +15,12 @@ export interface ButtonProps {
  * Component to render drop down input form element. Supports multi select and auto complete features
  */
 const Button = (props: ButtonProps) => {
-
-    const handleClick = (event: any) => {
-        console.log("****");
-    }
-
     return (
-        <button className={`basicui-button basicui-button--theme-${props.theme || ThemeType.default}`} type="button" onClick={handleClick}>
+        <button className={`basicui-button basicui-button--theme-${props.theme || ThemeType.default}`}
+            type={props.type || "button"}
+            onSubmit={props.onSubmit}
+            onReset={props.onReset}
+            onClick={props.onClick}>
             {props.children}
         </button>
     );
