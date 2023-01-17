dc5778787b2de83a257a5645cf52f02b3eeab40f
Arunkumar Selvaraj <arun@ioak.io>
2023-01-17 12:21:58 +0530

    fixes

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,6 @@ import Textarea from './components/Textarea';
 import SelectNative from './components/SelectNative';
 
 import * as SelectPropsConverter from './converters/Select';
+import ThemeType from "./components/types/ThemeType";
 
-export { Select, SelectPropsConverter, Input, Button, Link, Textarea, SelectNative };
+export { Select, SelectPropsConverter, Input, Button, Link, Textarea, SelectNative, ThemeType };
