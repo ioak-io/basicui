f267c036c0fe408fd816c72af8bcc93d6f5433d9
Arunkumar Selvaraj <arun@ioak.io>
2023-01-20 11:27:43 +0530

    Accordion component fix for stopping expansion / collapse until previos animation is finished

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -15,6 +15,13 @@ export interface AccordionProps {
  */
 const Accordion = (props: AccordionProps) => {
     const bodyRef: React.MutableRefObject<any> = useRef(null);
+    const isAnimationCompletedRef = useRef(true);
+
+    const handleChange = () => {
+        if (isAnimationCompletedRef.current) {
+            props.onChange();
+        }
+    }
 
     useEffect(() => {
         updateScrollHeight();
@@ -25,22 +32,25 @@ const Accordion = (props: AccordionProps) => {
         if (!bodyRef.current) return;
         //   setTimeout(() => {
         if (props.expanded && bodyRef.current) {
-            console.log("**", bodyRef.current.scrollHeight);
-            bodyRef.current.style.maxHeight = bodyRef.current.scrollHeight + 'px';
+            bodyRef.current.style.height = bodyRef.current.scrollHeight + 'px';
             bodyRef.current.style.overflowY = 'hidden';
             bodyRef.current.style.visibility = 'visible';
+            isAnimationCompletedRef.current = false;
             setTimeout(() => {
                 bodyRef.current.style.overflowY = 'visible';
-                bodyRef.current.style.maxHeight = 'none';
+                bodyRef.current.style.height = 'auto';
+                isAnimationCompletedRef.current = true;
             }, 300);
         } else if (bodyRef.current) {
-            bodyRef.current.style.maxHeight = bodyRef.current.scrollHeight + 'px';
+            isAnimationCompletedRef.current = false;
+            bodyRef.current.style.height = bodyRef.current.scrollHeight + 'px';
             setTimeout(() => {
-                bodyRef.current.style.maxHeight = '0px';
+                bodyRef.current.style.height = '0px';
             }, 0);
             bodyRef.current.style.overflowY = 'hidden';
             setTimeout(() => {
                 bodyRef.current.style.visibility = 'hidden';
+                isAnimationCompletedRef.current = true;
             }, 300);
         }
         //   }, 0);
@@ -48,7 +58,7 @@ const Accordion = (props: AccordionProps) => {
 
     return (
         <div className={`basicui-accordion  ${props.expanded ? "basicui-accordion--active" : ""} ${props.flushed ? "basicui-accordion--flushed" : ""}`}>
-            <button className="basicui-accordion__header" onClick={props.onChange}>
+            <button className="basicui-accordion__header" onClick={handleChange}>
                 <div>{props.heading}</div>
                 v
             </button>
