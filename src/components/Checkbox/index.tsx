d22c604a3f8115f0e7668d41c3ca31559d9eabfc
Arunkumar Selvaraj <arun@ioak.io>
2023-02-08 13:36:12 +0530

    simplyfication and pass through properties from checkbox components to it html primitives

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,27 +1,30 @@
 import React, { useState, useRef, useEffect } from "react";
 
 import "./style.css";
-import { isEmptyOrSpaces } from "../../utils/TextUtils";
-import FormElementMessage from "../shared/FormElementMessage";
 import ThemeType from "../types/ThemeType";
 
 export interface ChekboxProps {
     id: string;
-    value?: string | number;
-    checked?: boolean;
     label?: string;
-    tooltip?: string;
     theme?: ThemeType;
+    checked?: boolean;
+    [key: string]: any;
 };
 
 /**
  * Component to render checkbox form element. For using it with standard html input, add css class basicui-input
  */
-const Chekbox = (props: ChekboxProps) => {
+const Chekbox = ({
+    id,
+    label,
+    theme,
+    checked,
+    ...restProps
+}: ChekboxProps) => {
     return (
-        <label className={`basicui-checkbox ${props.checked ? "basicui-checkbox--checked" : ""} basicui-checkbox--theme-${props.theme || ThemeType.default}`} htmlFor={props.id}>
-            <input className="basicui-checkbox__input" id={props.id} checked={props.checked} type="checkbox" value={props.value} />
-            {props.label && <span className="basicui-checkbox__span">{props.label}</span>}
+        <label className={`basicui-checkbox ${checked ? "basicui-checkbox--checked" : ""} basicui-checkbox--theme-${theme || ThemeType.default}`} htmlFor={id}>
+            <input className="basicui-checkbox__input" id={id} checked={checked} type="checkbox" {...restProps} />
+            {label && <span className="basicui-checkbox__span">{label}</span>}
         </label>
     );
 };
