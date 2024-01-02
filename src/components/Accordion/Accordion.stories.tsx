a6b060a1c2e921d0c9d7551cc3abf2b898056281
Arunkumar Selvaraj <arun@ioak.io>
2024-01-02 12:17:36 +0530

    Refactored stories to latest storybook standards

--- src/components/Accordion/Accordion.stories.tsx
+++ src/components/Accordion/Accordion.stories.tsx
@@ -2,7 +2,6 @@ import { Meta, StoryObj } from '@storybook/react';
 import Accordion, { AccordionProps } from ".";
 import ThemeType from "../types/ThemeType";
 import AccordionWrapper from "./AccordionWrapper";
-import AccordionOverview from "./AccordionOverview";
 import React from 'react';
 
 const meta: Meta<typeof Accordion> = {
@@ -23,21 +22,6 @@ const Template: Story = {
   },
 };
 
-const TemplateOverview: Story = {
-  render: () => {
-    return (
-      <AccordionOverview />
-    );
-  },
-};
-
-export const Overview = {
-  ...TemplateOverview,
-  args: {
-    
-  },
-};
-
 export const Playground = {
   ...Template,
   args: {
@@ -45,18 +29,3 @@ export const Playground = {
     bordered: false
   },
 };
-
-// Create a master template for mapping args to render the Accordion component
-// const Template: Story = (args: AccordionProps) => <AccordionWrapper {...args} />;
-// const TemplateOverview: Story<AccordionProps> = () => <AccordionOverview />;
-
-// Reuse that template for creating different stories
-// export const Overview = TemplateOverview.bind({});
-// TemplateOverview.args = {
-//   bordered: false
-// };
-
-// export const Playground = Template.bind({});
-// Playground.args = {
-//   theme: ThemeType.primary
-// };
