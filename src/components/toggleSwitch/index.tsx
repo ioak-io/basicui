a37e68a645a356ca87d717a0253215493d310f07
Mohana krishnan <mohana.krishnan@westernacher.com>
2024-04-04 11:45:49 +0530

    color changes for different themes added

--- src/components/toggleSwitch/index.tsx
+++ src/components/toggleSwitch/index.tsx
@@ -14,22 +14,44 @@ export interface SwitchProps {
  * Component to render switch form element. For using it with standard html input, add css class basicui-input
  */
 const Switch = ({ id, label, theme, checked, ...restProps }: SwitchProps) => {
+  // return (
+  //   <label
+      // className={`basicui-switch ${
+      //   checked ? "basicui-switch--checked" : ""
+      // } basicui-switch--theme-${theme || ThemeType.default}`}
+  //     htmlFor={id}
+  //   >
+  //     <input
+  //       className="basicui-switch__input"
+  //       id={id}
+  //       checked={checked}
+  //       type="checkbox"
+  //       {...restProps}
+  //     />
+  //     <span className="basicui-switch__slider"></span>
+  //   </label>
+  // );
   return (
-    <label
-      className={`basicui-switch ${
-        checked ? "basicui-switch--checked" : ""
-      } basicui-switch--theme-${theme || ThemeType.default}`}
-      htmlFor={id}
-    >
-      <input
-        className="basicui-switch__input"
-        id={id}
-        checked={checked}
-        type="checkbox"
-        {...restProps}
-      />
-      <span className="basicui-switch__slider" />
-    </label>
+    <label className={`basicui-switch ${
+      checked ? "basicui-switch--checked" : ""
+    } basicui-switch--theme-${theme || ThemeType.default}`}  htmlFor={id}>
+	<span className="basicui-switch__input-wrap">
+		<input className={`basicui-switch__input  basicui-switch__input-${theme || ThemeType.default}`} 
+      id={id} checked={checked} type="checkbox" role="switch" value="dark" {...restProps}/>
+		<span className="basicui-switch__slider"></span>
+		<span className="basicui-switch__theme__icon">
+			<span className="basicui-switch__theme__icon-part"></span>
+			<span className="basicui-switch__theme__icon-part"></span>
+			<span className="basicui-switch__theme__icon-part"></span>
+			<span className="basicui-switch__theme__icon-part"></span>
+			<span className="basicui-switch__theme__icon-part"></span>
+			<span className="basicui-switch__theme__icon-part"></span>
+			<span className="basicui-switch__theme__icon-part"></span>
+			<span className="basicui-switch__theme__icon-part"></span>
+			<span className="basicui-switch__theme__icon-part"></span>
+		</span>
+	</span>
+</label>
   );
 };
 
